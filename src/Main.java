3326cf9e57d0af4ad4440f580fe89212a851e456
mesak <mrai3826@zenithacademy.org>
2026-03-03 15:18:05 -0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        int age;
+
+        System.out.print("Enter your age: ");
+
+        if (in.hasNextInt()) {
+            age = in.nextInt();
+
+            if (age >= 21) {
+                System.out.println("You get a paper wrist band.");
+            }
+        }
     }
 }
